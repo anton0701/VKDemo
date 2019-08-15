89a2e93c7580f6e18d17adb54d8743e3ecef995d
AntonKrylovBrightBox <akrylov@bright-box.com>
2019-08-15 19:43:46 +0300

    +

--- VKDemo/Core/UI/Custom Buttons/LikeButton.swift
+++ VKDemo/Core/UI/Custom Buttons/LikeButton.swift
@@ -9,12 +9,13 @@
 import Foundation
 import UIKit
 
+@IBDesignable
 class LikeButton: UIButton {
     private let fillColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
     private let blueMain = UIColor(red: 0.294, green: 0.471, blue: 0.706, alpha: 1.000)
 
     private lazy var likeLayer: CAShapeLayer = {
-        let layer = CAShapeLayer()
+        let likeLayer = CAShapeLayer()
         
         let bezierPath = UIBezierPath()
         bezierPath.move(to: CGPoint(x: frame.minX + 0.50000 * frame.width, y: frame.minY + 0.98298 * frame.height))
@@ -27,28 +28,42 @@ class LikeButton: UIButton {
         bezierPath.addCurve(to: CGPoint(x: frame.minX + 0.56808 * frame.width, y: frame.minY + 0.90900 * frame.height), controlPoint1: CGPoint(x: frame.minX + 0.98627 * frame.width, y: frame.minY + 0.50209 * frame.height), controlPoint2: CGPoint(x: frame.minX + 0.82094 * frame.width, y: frame.minY + 0.66591 * frame.height))
         bezierPath.addLine(to: CGPoint(x: frame.minX + 0.50000 * frame.width, y: frame.minY + 0.98298 * frame.height))
         bezierPath.close()
-        #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).setFill()
+        fillColor.setFill()
         bezierPath.fill()
         blueMain.setStroke()
         bezierPath.lineWidth = 2.5
         bezierPath.stroke()
 
-        layer.path = bezierPath.cgPath
-        return layer
+        likeLayer.path = bezierPath.cgPath
+        return likeLayer
     }()
     
     override init(frame: CGRect) {
       super.init(frame: frame)
-      configureLayers()
+      configureAppearance()
     }
     
     required init?(coder aDecoder: NSCoder) {
       super.init(coder: aDecoder)
-      configureLayers()
+      configureAppearance()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        configureAppearance()
+    }
+    
+    private func configureAppearance() {
+        configureLayers()
+        configOther()
     }
     
     private func configureLayers() {
-        backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
         layer.addSublayer(likeLayer)
+    }
+    
+    private func configOther() {
+        titleLabel?.text = ""
     }
 }
