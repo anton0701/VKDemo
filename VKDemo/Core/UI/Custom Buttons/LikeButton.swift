3038bcb7198e9c898e90a3c54faf9e58719e67dc
AntonKrylovBrightBox <akrylov@bright-box.com>
2019-08-15 20:40:24 +0300

    +

--- VKDemo/Core/UI/Custom Buttons/LikeButton.swift
+++ VKDemo/Core/UI/Custom Buttons/LikeButton.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 @IBDesignable
-class LikeButton: UIButton {
+class LikeButton: UIControl {
     private let fillColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
     private let blueMain = UIColor(red: 0.294, green: 0.471, blue: 0.706, alpha: 1.000)
 
@@ -18,15 +18,16 @@ class LikeButton: UIButton {
         let likeLayer = CAShapeLayer()
         
         let bezierPath = UIBezierPath()
-        bezierPath.move(to: CGPoint(x: frame.minX + 0.50000 * frame.width, y: frame.minY + 0.98298 * frame.height))
-        bezierPath.addLine(to: CGPoint(x: frame.minX + 0.43192 * frame.width, y: frame.minY + 0.90900 * frame.height))
-        bezierPath.addCurve(to: CGPoint(x: frame.minX + 0.01373 * frame.width, y: frame.minY + 0.30128 * frame.height), controlPoint1: CGPoint(x: frame.minX + 0.17906 * frame.width, y: frame.minY + 0.66591 * frame.height), controlPoint2: CGPoint(x: frame.minX + 0.01373 * frame.width, y: frame.minY + 0.50209 * frame.height))
-        bezierPath.addCurve(to: CGPoint(x: frame.minX + 0.28118 * frame.width, y: frame.minY + 0.01064 * frame.height), controlPoint1: CGPoint(x: frame.minX + 0.01373 * frame.width, y: frame.minY + 0.13747 * frame.height), controlPoint2: CGPoint(x: frame.minX + 0.13043 * frame.width, y: frame.minY + 0.01064 * frame.height))
-        bezierPath.addCurve(to: CGPoint(x: frame.minX + 0.50000 * frame.width, y: frame.minY + 0.12161 * frame.height), controlPoint1: CGPoint(x: frame.minX + 0.36384 * frame.width, y: frame.minY + 0.01064 * frame.height), controlPoint2: CGPoint(x: frame.minX + 0.44651 * frame.width, y: frame.minY + 0.05291 * frame.height))
-        bezierPath.addCurve(to: CGPoint(x: frame.minX + 0.71882 * frame.width, y: frame.minY + 0.01064 * frame.height), controlPoint1: CGPoint(x: frame.minX + 0.55349 * frame.width, y: frame.minY + 0.05291 * frame.height), controlPoint2: CGPoint(x: frame.minX + 0.63616 * frame.width, y: frame.minY + 0.01064 * frame.height))
-        bezierPath.addCurve(to: CGPoint(x: frame.minX + 0.98627 * frame.width, y: frame.minY + 0.30128 * frame.height), controlPoint1: CGPoint(x: frame.minX + 0.86957 * frame.width, y: frame.minY + 0.01064 * frame.height), controlPoint2: CGPoint(x: frame.minX + 0.98627 * frame.width, y: frame.minY + 0.13747 * frame.height))
-        bezierPath.addCurve(to: CGPoint(x: frame.minX + 0.56808 * frame.width, y: frame.minY + 0.90900 * frame.height), controlPoint1: CGPoint(x: frame.minX + 0.98627 * frame.width, y: frame.minY + 0.50209 * frame.height), controlPoint2: CGPoint(x: frame.minX + 0.82094 * frame.width, y: frame.minY + 0.66591 * frame.height))
-        bezierPath.addLine(to: CGPoint(x: frame.minX + 0.50000 * frame.width, y: frame.minY + 0.98298 * frame.height))
+        bezierPath.move(to: CGPoint(x: bounds.minX + 0.50000 * bounds.width, y: bounds.minY + 0.98298 * bounds.height))
+        bezierPath.addLine(to: CGPoint(x: bounds.minX + 0.43192 * bounds.width, y: bounds.minY + 0.90900 * bounds.height))
+        bezierPath.addCurve(to: CGPoint(x: bounds.minX + 0.01373 * bounds.width, y: bounds.minY + 0.30128 * bounds.height), controlPoint1: CGPoint(x: bounds.minX + 0.17906 * bounds.width, y: bounds.minY + 0.66591 * bounds.height), controlPoint2: CGPoint(x: bounds.minX + 0.01373 * bounds.width, y: bounds.minY + 0.50209 * bounds.height))
+        bezierPath.addCurve(to: CGPoint(x: bounds.minX + 0.28118 * bounds.width, y: bounds.minY + 0.01064 * bounds.height), controlPoint1: CGPoint(x: bounds.minX + 0.01373 * bounds.width, y: bounds.minY + 0.13747 * bounds.height), controlPoint2: CGPoint(x: bounds.minX + 0.13043 * bounds.width, y: bounds.minY + 0.01064 * bounds.height))
+        bezierPath.addCurve(to: CGPoint(x: bounds.minX + 0.50000 * bounds.width, y: bounds.minY + 0.12161 * bounds.height), controlPoint1: CGPoint(x: bounds.minX + 0.36384 * bounds.width, y: bounds.minY + 0.01064 * bounds.height), controlPoint2: CGPoint(x: bounds.minX + 0.44651 * bounds.width, y: bounds.minY + 0.05291 * bounds.height))
+        bezierPath.addCurve(to: CGPoint(x: bounds.minX + 0.71882 * bounds.width, y: bounds.minY + 0.01064 * bounds.height), controlPoint1: CGPoint(x: bounds.minX + 0.55349 * bounds.width, y: bounds.minY + 0.05291 * bounds.height), controlPoint2: CGPoint(x: bounds.minX + 0.63616 * bounds.width, y: bounds.minY + 0.01064 * bounds.height))
+        bezierPath.addCurve(to: CGPoint(x: bounds.minX + 0.98627 * bounds.width, y: bounds.minY + 0.30128 * bounds.height), controlPoint1: CGPoint(x: bounds.minX + 0.86957 * bounds.width, y: bounds.minY + 0.01064 * bounds.height), controlPoint2: CGPoint(x: bounds.minX + 0.98627 * bounds.width, y: bounds.minY + 0.13747 * bounds.height))
+        bezierPath.addCurve(to: CGPoint(x: bounds.minX + 0.56808 * bounds.width, y: bounds.minY + 0.90900 * bounds.height), controlPoint1: CGPoint(x: bounds.minX + 0.98627 * bounds.width, y: bounds.minY + 0.50209 * bounds.height), controlPoint2: CGPoint(x: bounds.minX + 0.82094 * bounds.width, y: bounds.minY + 0.66591 * bounds.height))
+        bezierPath.addLine(to: CGPoint(x: bounds.minX + 0.50000 * bounds.width, y: bounds.minY + 0.98298 * bounds.height))
+
         bezierPath.close()
         fillColor.setFill()
         bezierPath.fill()
@@ -37,15 +38,15 @@ class LikeButton: UIButton {
         likeLayer.path = bezierPath.cgPath
         return likeLayer
     }()
-    
+        
     override init(frame: CGRect) {
       super.init(frame: frame)
-      configureAppearance()
+//      configureAppearance()
     }
     
     required init?(coder aDecoder: NSCoder) {
       super.init(coder: aDecoder)
-      configureAppearance()
+//      configureAppearance()
     }
     
     override func prepareForInterfaceBuilder() {
@@ -59,11 +60,25 @@ class LikeButton: UIButton {
     }
     
     private func configureLayers() {
-        backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+        backgroundColor = .clear
+        likeLayer.frame = bounds
         layer.addSublayer(likeLayer)
+        likeLayer.setNeedsDisplay()
     }
     
     private func configOther() {
-        titleLabel?.text = ""
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+//        configureAppearance()
+    }
+        
+    override var frame: CGRect {
+        didSet {
+            configureAppearance()
+        }
     }
 }
