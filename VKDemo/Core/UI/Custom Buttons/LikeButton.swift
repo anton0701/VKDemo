62c221af47c7816497dc6976f0fc32ec17c53a94
AntonKrylovBrightBox <akrylov@bright-box.com>
2019-08-19 23:53:54 +0300

    LikeButton

--- VKDemo/Core/UI/Custom Buttons/LikeButton.swift
+++ VKDemo/Core/UI/Custom Buttons/LikeButton.swift
@@ -13,7 +13,10 @@ import QuartzCore
 @IBDesignable
 class LikeButton: UIControl {
     
-    private let fillColor = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1.000)
+//    private let fillColor = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1.000)
+    let fillColor = UIColor(red: 0.667, green: 0.667, blue: 0.667, alpha: 1.000)
+    let heartStrokeColor = #colorLiteral(red: 0.6666666667, green: 0.6666666667, blue: 0.6666666667, alpha: 0.5)
+    let borderStrokeColor = #colorLiteral(red: 0.6666666667, green: 0.6666666667, blue: 0.6666666667, alpha: 0.5)
     private let likedFillColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
     private let notLikedFillColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     private let blueMain = UIColor(red: 0.294, green: 0.471, blue: 0.706, alpha: 1.000)
@@ -38,28 +41,28 @@ class LikeButton: UIControl {
     }
     
     private lazy var heartLayer: CAShapeLayer = {
-        let bezierPath = UIBezierPath()
-        bezierPath.move(to: CGPoint(x: 50, y: 81))
-        bezierPath.addLine(to: CGPoint(x: 45.66, y: 76.97))
-        bezierPath.addCurve(to: CGPoint(x: 19, y: 41.81), controlPoint1: CGPoint(x: 29.54, y: 62.91), controlPoint2: CGPoint(x: 19, y: 53.43))
-        bezierPath.addCurve(to: CGPoint(x: 36.05, y: 25), controlPoint1: CGPoint(x: 19, y: 32.34), controlPoint2: CGPoint(x: 26.44, y: 25))
-        bezierPath.addCurve(to: CGPoint(x: 50, y: 31.42), controlPoint1: CGPoint(x: 41.32, y: 25), controlPoint2: CGPoint(x: 46.59, y: 27.45))
-        bezierPath.addCurve(to: CGPoint(x: 63.95, y: 25), controlPoint1: CGPoint(x: 53.41, y: 27.45), controlPoint2: CGPoint(x: 58.68, y: 25))
-        bezierPath.addCurve(to: CGPoint(x: 81, y: 41.81), controlPoint1: CGPoint(x: 73.56, y: 25), controlPoint2: CGPoint(x: 81, y: 32.34))
-        bezierPath.addCurve(to: CGPoint(x: 54.34, y: 76.97), controlPoint1: CGPoint(x: 81, y: 53.43), controlPoint2: CGPoint(x: 70.46, y: 62.91))
-        bezierPath.addLine(to: CGPoint(x: 50, y: 81))
-        bezierPath.close()
-        UIColor.red.setFill()
-        bezierPath.fill()
-        UIColor.lightGray.setStroke()
-        bezierPath.lineWidth = 6.5
-        bezierPath.miterLimit = 4
-        bezierPath.stroke()
+        let heartPath = UIBezierPath()
+        heartPath.move(to: CGPoint(x: 21.85, y: 34))
+        heartPath.addLine(to: CGPoint(x: 20.17, y: 32.27))
+        heartPath.addCurve(to: CGPoint(x: 9.85, y: 17.21), controlPoint1: CGPoint(x: 13.93, y: 26.25), controlPoint2: CGPoint(x: 9.85, y: 22.18))
+        heartPath.addCurve(to: CGPoint(x: 16.45, y: 10), controlPoint1: CGPoint(x: 9.85, y: 13.14), controlPoint2: CGPoint(x: 12.73, y: 10))
+        heartPath.addCurve(to: CGPoint(x: 21.85, y: 12.75), controlPoint1: CGPoint(x: 18.49, y: 10), controlPoint2: CGPoint(x: 20.53, y: 11.05))
+        heartPath.addCurve(to: CGPoint(x: 27.25, y: 10), controlPoint1: CGPoint(x: 23.17, y: 11.05), controlPoint2: CGPoint(x: 25.21, y: 10))
+        heartPath.addCurve(to: CGPoint(x: 33.85, y: 17.21), controlPoint1: CGPoint(x: 30.97, y: 10), controlPoint2: CGPoint(x: 33.85, y: 13.14))
+        heartPath.addCurve(to: CGPoint(x: 23.53, y: 32.27), controlPoint1: CGPoint(x: 33.85, y: 22.18), controlPoint2: CGPoint(x: 29.77, y: 26.25))
+        heartPath.addLine(to: CGPoint(x: 21.85, y: 34))
+        heartPath.close()
+        #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).setFill()
+        heartPath.fill()
+        heartStrokeColor.setStroke()
+        heartPath.lineCapStyle = .round
+        heartPath.stroke()
 
         let layer = CAShapeLayer()
-        layer.path = bezierPath.cgPath
+        layer.path = heartPath.cgPath
         layer.fillColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).cgColor
-        layer.strokeColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1).cgColor
+        layer.strokeColor = heartStrokeColor.cgColor
+        layer.lineWidth = 2.0
         
         layer.frame = bounds
         
@@ -68,19 +71,17 @@ class LikeButton: UIControl {
     
     private lazy var borderLayer: CAShapeLayer = {
         let layer = CAShapeLayer()
-        let rectanglePath = UIBezierPath(roundedRect: CGRect(x: 4, y: 7, width: 201.5, height: 91.5), cornerRadius: 39)
+        let rectanglePath = UIBezierPath(roundedRect: CGRect(x: 1, y: 1, width: 93, height: 42), cornerRadius: 21)
         fillColor.setStroke()
-        #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).setFill()
-        rectanglePath.fill()
-        rectanglePath.stroke()
-        rectanglePath.lineWidth = 3
+        rectanglePath.lineWidth = 0.5
         rectanglePath.lineCapStyle = .round
         rectanglePath.lineJoinStyle = .round
         rectanglePath.stroke()
 
         layer.path = rectanglePath.cgPath
-        layer.strokeColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).cgColor
-        layer.fillColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).cgColor
+        layer.strokeColor = borderStrokeColor.cgColor
+        layer.fillColor = UIColor.clear.cgColor
+        layer.lineWidth = 0.5
         
         layer.frame = bounds
         
@@ -115,16 +116,4 @@ class LikeButton: UIControl {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
-//    override func draw(_ rect: CGRect) {
-//        configureAppearance()
-//
-//        for layer in [
-//          borderLayer,
-//          heartLayer
-//        ] {
-//          layer.bounds = bounds
-//          layer.position = center
-//        }
-//    }
 }
