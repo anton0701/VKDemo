5c82d508d97e7a038756abb140a8e57f4a51bfe7
AntonKrylovBrightBox <akrylov@bright-box.com>
2019-08-22 08:21:15 +0300

    + UserDto

--- VKDemo/Core/Models/Codable/Dto/UserDto.swift
+++ VKDemo/Core/Models/Codable/Dto/UserDto.swift
@@ -12,16 +12,30 @@ struct UserDto: Codable {
     let id: Int
     let firstName: String
     let lastName: String
-    let deactivated: String?
     let isClosed: Bool
     let canAccessClosed: Bool
+    let sex: Sex
+    let screenName: String
+    let photo50: String
+    let photo100: String
+    let online: BoolInt
+    
+    enum Sex: Int, Codable {
+        case unknown = 0
+        case male
+        case female
+    }
     
     private enum CodingKeys: String, CodingKey {
         case id
         case firstName = "first_name"
         case lastName = "last_name"
-        case deactivated
         case isClosed = "is_closed"
         case canAccessClosed = "can_access_closed"
+        case sex
+        case screenName = "screen_name"
+        case photo50 = "photo_50"
+        case photo100 = "photo_100"
+        case online
     }
 }
