b0f23efd06d961dd37ebcd10b787b54a0d0db000
AntonKrylovBrightBox <akrylov@bright-box.com>
2019-11-01 03:04:49 +0300

    +

--- VKDemo/Core/Dependencies/Services/Messages/MessagesTarget.swift
+++ VKDemo/Core/Dependencies/Services/Messages/MessagesTarget.swift
@@ -13,3 +13,54 @@ import Alamofire
 enum MessagesTarget {
     case getConversations
 }
+
+extension MessagesTarget: TargetType {
+    
+    private var sessionManager: IUserSessionManager {
+        return  DI.container.core.sessionManager
+    }
+    
+    var baseURL: URL {
+        return URL(string: "https://api.vk.com/method/")!
+    }
+    
+    var path: String {
+        switch self {
+        case .getConversations:
+            return "messages.getConversations"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        default:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .getConversations:
+            return .requestParameters(parameters: ["v": 5.95, "access_token": sessionManager.getAccessToken() ?? "", "count" : "200"], encoding: URLEncoding.queryString)
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+}
+
+extension MessagesTarget: VKAccessTokenAuthorizable {
+    var authorizationType: VKAuthorizationType {
+        return .oauth
+    }
+}
